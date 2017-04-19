ec4745a57c82fd342e488382eaff7d6c7c7fd8ef
Chris <christoph.kralj@gmail.com>
2017-04-19 19:23:04 +0200

    Updated analysis script

--- analysis.py
+++ analysis.py
@@ -5,20 +5,22 @@ from gensim.models import Word2Vec
 from gensim.models.wrappers import FastText
 
 
-idfs = pd.read_csv("datasets/idf_scores.csv")
+# idfs = pd.read_csv("datasets/idf_scores.csv")
 counts = pd.read_csv("datasets/counts.csv")
+n_counts = pd.read_csv("datasets/neighbour_counts.csv")
 wv_model = Word2Vec.load('datasets/wv_model')
 ft_model = FastText.load("datasets/ft_model")
 
 wv_model.most_similar("jazz")
-top = ft_model.most_similar("jazz", topn=1000)
+top = ft_model.most_similar("jazz", topn=200)
 
-df = pd.DataFrame(columns=('tag_name', 'sim', 'count'))
+df = pd.DataFrame(columns=('tag_name', 'sim', 'count', 'ncount'))
 
 for i, row in enumerate(top):
     tag = row[0]
     sim = row[1]
     count = counts[counts.tag_name == tag]["count"].values[0]
-    # idf = idfs[idfs.feature == tag]["idf"].values[0]
+    ncount = n_counts[n_counts.tag_name == tag]["count"].values[0]
 
-    df.loc[i] = [tag, sim, count]
+    if count <= 5:
+        df.loc[len(df)] = [tag, sim, count, ncount]
