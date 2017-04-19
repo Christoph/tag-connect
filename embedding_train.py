fb2364d68277f67274737874af86a020ff883b0f
Chris <christoph.kralj@gmail.com>
2017-04-19 19:20:59 +0200

    Added neighbour creation script

--- embedding_train.py
+++ embedding_train.py
@@ -7,6 +7,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 
 
 data = pd.read_csv("datasets/tag_docs.csv")
+data.tag_name = data.tag_name.astype(str)
 subset = data[0:10000]
 
 used_dataset = data.tag_doc.str.lower()
@@ -27,6 +28,7 @@ for song in docs:
         else:
             counts.setdefault(tag, 1)
 
+
 tag_counts = pd.DataFrame.from_dict(counts, orient="index").reset_index()
 tag_counts.columns = ["tag_name", "count"]
 wv_model = Word2Vec(sentences, window=5, min_count=1, workers=4)
@@ -44,8 +46,30 @@ feats = pd.DataFrame({
 feats = feats.sort_values(
     "idf", ascending=False)
 
+number_of_top_tags = 10
+n_counts = {}
+
+# Create neighborhood for the tags
+for ind, tag in data.tag_name.iteritems():
+    # Get neighbours
+    neighs = ft_model.most_similar(tag, topn=number_of_top_tags)
+
+    # Add center tag
+    neighs.append((tag, 1.0))
+
+    # Get neighbour counts
+    for n in [a[0] for a in neighs]:
+        if n in n_counts:
+            n_counts[n] += 1
+        else:
+            n_counts.setdefault(n, 1)
+
+neigh_counts = pd.DataFrame.from_dict(n_counts, orient="index").reset_index()
+neigh_counts.columns = ["tag_name", "neighbour_count"]
+
 # ! Already saved complete models in datasets/ !
 feats.to_csv("datasets/idf_scores.csv", index=False)
 tag_counts.to_csv("datasets/counts.csv", index=False)
+neigh_counts.to_csv("datasets/neighbour_counts.csv", index=False)
 wv_model.save("datasets/wv_model")
 ft_model.save("datasets/ft_model")
