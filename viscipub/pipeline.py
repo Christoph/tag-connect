72aa6137b63ae0ebbf78b44f66749f0de7f3f646
Christoph Kralj <christoph.kralj@gmail.com>
2018-08-30 17:28:43 +0200

    Imported relevant data

--- viscipub/pipeline.py
+++ viscipub/pipeline.py
@@ -1,3 +1,11 @@
 import pandas as pd
 
-data = pd.read_csv("")
+raw = pd.read_csv("../datasets/keyvis.csv")
+
+raw = raw.drop(range(0, len(raw), 2))
+raw = raw.reset_index()
+raw = raw.drop(["index", "Paper ID"], axis=1)
+
+data = raw[["inspec", "inspec_controlled", "ieee_terms", "author_keywords", "real_author_keywords", "abstract"]]
+
+data
