e6741d96d72a6983cc4f9161c83da6901607470f
Christoph Kralj <christoph.kralj@gmail.com>
2018-11-28 23:44:00 +0100

    Lda runs was successful and new vector files are added to the embedding comparison project

--- keyvis_add/lda_creator_auto.py
+++ keyvis_add/lda_creator_auto.py
@@ -77,6 +77,14 @@ def lemmatization(text, stopwords):
                           not token.lemma_ == "-PRON-"])
     return texts_out
 
+def raw_stopwords(text, stopwords):
+    """https://spacy.io/api/annotation"""
+    texts_out = []
+    for sent in text.sents:
+        texts_out.extend([token.text for token in sent if
+                          token.lemma_ not in stop_words])
+    return texts_out
+
 # DATA Loading
 raw = np.load("../datasets/full.pkl")
 
@@ -87,11 +95,12 @@ abstracts = [nlp(text) for text in raw["Abstract"].tolist()]
 abstract_lemma = [lemmatization(ab, [stopwords]) for ab in abstracts]
 
 docs = [nlp(text) for text in raw["Fulltext"].tolist()]
+full_raw = [raw_stopwords(doc, stop_words) for doc in docs]
 full_lemma = [lemmatization(doc, stop_words) for doc in docs]
 full_clean = [preprocessing(doc, stop_words) for doc in docs]
 
-# id2word = corpora.Dictionary(abstract_lemma)
-# corpus = [id2word.doc2bow(text) for text in abstract_lemma]
+# id2word = corpora.Dictionary(full_clean)
+# corpus = [id2word.doc2bow(text) for text in full_clean]
 #
 # model = gensim.models.ldamodel.LdaModel(corpus=corpus,
 #                                         id2word=id2word,
@@ -105,11 +114,11 @@ full_clean = [preprocessing(doc, stop_words) for doc in docs]
 # vec = [gensim.matutils.sparse2full(spa, 5) for spa in model[corpus]]
 
 # params
-n_dim = [4,6,8,10,12,15,20]
+n_dim = [4,8,12,18,24,30,50]
 algorithms = ["lda", "nmf"]
-datasets = ["lemma_abstract", "lemma_fulltext", "clean_fulltext"]
+datasets = ["lemma_abstract", "lemma_fulltext", "clean_fulltext", "full_raw"]
 
-n_runs_per_setting = 10
+n_runs_per_setting = 5
 topic_consistency_thresholds = [5, 10, 20]
 
 # Create all parameter permutations
@@ -124,6 +133,13 @@ for run in runs:
     row = []
 
     # Set variables
+    if run[1] == "full_raw":
+        texts = [" ".join(text) for text in full_raw]
+        tfidf = TfidfVectorizer().fit(texts)
+        used = tfidf.transform(texts)
+
+        id2word = corpora.Dictionary(full_raw)
+        corpus = [id2word.doc2bow(text) for text in full_raw]
     if run[1] == "lemma_abstract":
         texts = [" ".join(text) for text in abstract_lemma]
         tfidf = TfidfVectorizer().fit(texts)
@@ -214,8 +230,34 @@ for run in runs:
 
 protocol.to_csv("runs.csv")
 
+# NMF
+texts = [" ".join(text) for text in full_lemma]
+tfidf = TfidfVectorizer().fit(texts)
+used = tfidf.transform(texts)
+model = NMF(int(10))
+vec = model.fit_transform(used)
+
+# LDA
+id2word = corpora.Dictionary(full_clean)
+corpus = [id2word.doc2bow(text) for text in full_clean]
+model = gensim.models.ldamodel.LdaModel(corpus=corpus,
+                                        id2word=id2word,
+                                        num_topics=20,
+                                        update_every=1,
+                                        chunksize=100,
+                                        passes=10,
+                                        alpha='auto',
+                                        per_word_topics=False)
+
+vec = [gensim.matutils.sparse2full(spa, 20) for spa in model[corpus]]
+
+save_vec(vec, "lda_clean_full_20.csv")
+
+similarity = cosine_similarity(vec)
+print_top_words(model, tfidf.get_feature_names(), 10)
+
 # Filter dimensions
-# np.median(vecs1, axis=0)
+np.median(vec, axis=0)
 # np.std(vecs1, axis=0)
 
 # print_top_words(ldas[1], tfidf.get_feature_names(), 10)
@@ -223,6 +265,14 @@ protocol.to_csv("runs.csv")
 
 # Create embeddings
 dois = raw["DOI"]
+
+
+def save_vec(vec, name):
+    vec = np.asarray(vec, dtype=np.object)
+    vec = np.insert(vec, 0, dois, axis=1)
+
+    emb1 = pd.DataFrame(vec)
+    emb1.to_csv(name, header=False, index=False)
 
 # LDA_NMF data
 lda = LatentDirichletAllocation(6, learning_method="batch")
