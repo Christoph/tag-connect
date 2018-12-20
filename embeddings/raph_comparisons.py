8d3edc4b36a6f040fc966ff86229b4d31ad29bba
Christoph <christoph.kralj@gmail.com>
2018-12-20 09:20:34 +0100

    Added raphaels latest changes

--- embeddings/raph_comparisons.py
+++ embeddings/raph_comparisons.py
@@ -26,6 +26,7 @@ en_dist = np.load('../datasets/endist.npy')
 em_dist = np.load('../datasets/emdist.npy')
 dlev_dist = np.load('../datasets/dlevdist.npy')
 raph_dist = np.load('../datasets/raphdist.npy')
+raphim_dist = np.load('../datasets/raphimdist.npy')
 # dist = np.load('../datasets/raphdist.npy')
 
 reload(embedding)
@@ -44,14 +45,16 @@ ll = [int(l) for l in labels]
 
 en_dist = (en_dist - en_dist.min()) / (en_dist.max() - en_dist.min())
 em_dist = (em_dist - em_dist.min()) / (em_dist.max() - em_dist.min())
-raph_dist = (raph_dist - raph_dist.min()) / (raph_dist.max() - raph_dist.min())
+# raph_dist = (raph_dist - raph_dist.min()) / (raph_dist.max() - raph_dist.min())
+# raphim_dist = (raphim_dist - raphim_dist.min()) / (raphim_dist.max() - raphim_dist.min())
 # sim = 1 - dist
 
-can = DBSCAN().fit_predict(em_dist)
+can = DBSCAN().fit_predict(raphim_dist)
 metrics.adjusted_rand_score(can, ll)
 
 # VIS
-vis.simMatrix(raph_dist)
+vis.simMatrixSensitive(raph_dist)
+vis.simMatrix(raphim_dist)
 # vis.graph(embedding.graph_from_sim(sim, 4))
 # vis.graph(embedding.graph_from_sim(sim, sim.mean()))
 vis.cluster_heatmap(
--- methods/forchristoph.py
+++ methods/forchristoph.py
@@ -1,4 +1,5 @@
 import copy
+import abstract
 import numpy as np
 from scipy.stats import wasserstein_distance
 from scipy.stats import energy_distance
@@ -233,6 +234,88 @@ class RaphMetric(AbstractMetric):
             out.append(np.subtract(semesterVector, el))
         return np.transpose(np.sign(out))
 
+class RaphMetricImproved(abstract.AbstractMetric):
+    def __init__(self, paths):
+        self.distanceMatrix = None
+        self.distanceDict   = dict()
+        self.paths          = sorted(paths, key=lambda x: x.label)
+        self.pathNames      = sorted(list(map(lambda x: x.label, paths)))
+        self.allCourseNames = abstract.extractAllCourseNames(paths)
+        np.set_printoptions(suppress=True)
+
+    def calculateDistanceDict(self):
+        #create array of all courses, sorted
+        #create vector from lookuptable (sorted courses)
+        #vector - each value as new internal representation matrix
+        #transpose before output
+        self.distanceDict = dict()
+        for path in self.paths:
+            distRow = dict()
+            for path2 in self.paths:
+                if path.label == path2.label:
+                    distRow[path2.label] = 0
+                else:
+                    distRow[path2.label] = self.calculatePathDistance(path, path2)
+            self.distanceDict[path.label] = distRow
+        return self.distanceDict
+
+    def calculateDistance(self):
+        self.distanceMatrix = None
+        for i, path in enumerate(self.paths):
+            distRow = np.array([])
+            for j, path2 in enumerate(self.paths):
+                if j <= i:
+                    distRow = np.append(distRow, [0])
+                else:
+                    distRow = np.append(distRow, [self.calculatePathDistance(path, path2)])
+            if self.distanceMatrix is None:
+                self.distanceMatrix = np.array([distRow])
+            else:
+                self.distanceMatrix = np.vstack((self.distanceMatrix, distRow))
+        #i_lower = np.tril_indices(len(self.pathNames), -1)
+        #self.distanceMatrix[i_lower] = self.distanceMatrix.T[i_lower]
+        return self.distanceMatrix, self.pathNames
+
+    def calculatePathDistance(self, pathA, pathB):
+        coursesA = abstract.extractAllCourseNames([pathA])
+        coursesB = abstract.extractAllCourseNames([pathB])
+        courseNames = abstract.extractAllCourseNames([pathA, pathB])
+        semesterA, semesterB = abstract.createExtendedLookUpList(pathA, pathB, courseNames)
+        distanceA = self.generateDistanceMatrix(semesterA)
+        distanceB = self.generateDistanceMatrix(semesterB)
+        distanceA = self.setNonOverlap(distanceA, courseNames, coursesA, 0)
+        distanceB = self.setNonOverlap(distanceB, courseNames, coursesB, 1)
+        distanceDiff = np.absolute(np.sign(np.subtract(distanceA, distanceB)))
+
+
+        n, m = distanceDiff.shape
+        size = n - 1
+        tri = np.fliplr(distanceDiff)[np.triu_indices(size)]
+        distance = sum(tri)/len(tri)
+        return distance
+
+    def setNonOverlap(self, distanceMatrix, courseNames, ownCourses, val):
+        for i, course in enumerate(courseNames):
+            if course not in ownCourses:
+                self.replaceRowCol(distanceMatrix, i, val)
+        return distanceMatrix
+
+    def replaceRowCol(self, M, i, val):
+        nrows, ncols = M.shape
+        #if(index>nrows or index>ncols) return
+        x = np.full([1, ncols], val)
+        y = np.full([nrows, 1], val)
+        M[i,:] = x
+        M[:,i:(i+1)] = y
+        np.fill_diagonal(M, 0)
+        return M
+
+    def generateDistanceMatrix(self, semesterVector):
+        out = []
+        for el in semesterVector:
+            out.append(np.subtract(semesterVector, el))
+        return np.transpose(np.sign(out))
+
 class EarthMoversMetric(AbstractMetric):
     def __init__(self, paths):
         self.distanceMatrix = None
@@ -324,7 +407,11 @@ class EnergyDistanceMetric(AbstractMetric):
 
 def raphRaphDistFun(pathA, pathB):
     metric = RaphMetric([pathA, pathB])
-    return float(metric.calculatePathDistance(pathA, pathB))
+    return metric.calculatePathDistance(pathA, pathB)
+
+def raphRaphImDistFun(pathA, pathB):
+    metric = RaphMetricImproved([pathA, pathB])
+    return metric.calculatePathDistance(pathA, pathB)
 
 def raphEarthDistFun(pathA, pathB):
     metric = EarthMoversMetric([pathA, pathB])
