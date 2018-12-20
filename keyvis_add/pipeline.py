0fa11ec8f2bc946d37a36979af10fb30cc328558
Christoph <christoph.kralj@gmail.com>
2018-12-20 12:55:52 +0100

    save topic models

--- keyvis_add/pipeline.py
+++ keyvis_add/pipeline.py
@@ -7,6 +7,10 @@ from sklearn.decomposition import NMF
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from scipy.spatial.distance import pdist
 from sklearn.preprocessing import MultiLabelBinarizer
+from nltk.corpus import stopwords
+import spacy
+
+nlp = spacy.load('en_core_web_md', disable=['ner'])
 
 # Add general functions to the project
 from os import path
@@ -15,6 +19,17 @@ sys.path.append(path.abspath('../methods'))
 
 import embedding
 import vis
+
+# Helper functions
+def lemmatization(text, stopwords):
+    """https://spacy.io/api/annotation"""
+    texts_out = []
+    for sent in text.sents:
+        texts_out.extend([token.lemma_ for token in sent if
+                          token.lemma_ not in stop_words and
+                          not token.lemma_ == "-PRON-"])
+    return texts_out
+
 
 # DATA Loading
 raw = np.load("../datasets/full.pkl")
@@ -33,17 +48,42 @@ meta = raw.drop(["Fulltext"], axis=1)
 meta['type'] = meta.apply(lambda row: 'new' if any(x in row["DOI"] for x in ["2013", "2012"]) else 'old', axis=1)
 meta.to_json("meta.json", orient="index")
 
-fulltext = raw["Fulltext"]
-fulltext.to_json("fulltext.json", orient="index")
+meta_fulltext = raw["Fulltext"]
+meta_fulltext.to_json("fulltext.json", orient="index")
 
 # EMBEDDINGS
-abstract_tfidf = TfidfVectorizer(stop_words="english").fit(raw["Abstract"])
-abstract = abstract_tfidf.transform(raw["Abstract"])
+stop_words = stopwords.words('english')
 
-fulltext_tfidf = TfidfVectorizer(stop_words="english").fit(raw["Fulltext"])
-fulltext = fulltext_tfidf.transform(raw["Fulltext"])
+abstracts = [nlp(text) for text in raw["Abstract"].tolist()]
+abstract_lemma = [lemmatization(ab, [stopwords]) for ab in abstracts]
 
-a_nmf = NMF(10).fit(fulltext)
+docs = [nlp(text) for text in raw["Fulltext"].tolist()]
+full_lemma = [lemmatization(doc, stop_words) for doc in docs]
+
+abstract_texts = [" ".join(text) for text in abstract_lemma]
+abstract_tfidf = TfidfVectorizer().fit(abstract_texts)
+abstract = abstract_tfidf.transform(abstract_texts)
+
+fulltext_texts = [" ".join(text) for text in full_lemma]
+fulltext_tfidf = TfidfVectorizer().fit(fulltext_texts)
+fulltext = abstract_tfidf.transform(fulltext_texts)
+
+abstract_nmf = NMF(10)
+abstract_vecs = abstract_nmf.fit_transform(abstract)
+
+full_nmf = NMF(10)
+full_vecs = full_nmf.fit_transform(fulltext)
+
+abstract_vecs = np.asarray(abstract_vecs, dtype=np.object)
+full_vecs = np.asarray(full_vecs, dtype=np.object)
+
+emb_abstract = pd.DataFrame(abstract_vecs)
+emb_abstract.to_json("nmf_abstract.json", orient="index")
+
+emb_full = pd.DataFrame(full_vecs)
+emb_full.to_json("nmf_full.json", orient="index")
+
+
 
 # CLASSIFICATION
 # train/test split for classification
