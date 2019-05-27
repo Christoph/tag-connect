9abf422e91006b7a13aa1a7de8c025cc4d24a60b
Christoph Kralj <christoph.kralj@gmail.com>
2019-05-27 20:45:53 +0200

    create upset plot function

--- keyvis_add/upset_vis.py
+++ keyvis_add/upset_vis.py
@@ -4,31 +4,33 @@ import pandas as pd
 import numpy as np
 from sklearn.preprocessing import MultiLabelBinarizer
 
-# action: Dataframe
-test_data = np.load("datasets/test_data_dict.pckl")
 meta = pd.read_json("datasets/meta.json", orient="index").sort_index()
-clusters = [list(filter(None, cluster.split(";"))) for cluster in meta["Clusters"].tolist()]
+clusters = np.array([list(filter(None, cluster.split(";"))) for cluster in meta["Clusters"].tolist()])
 titles = meta["Title"].tolist()
 
 enc = MultiLabelBinarizer()
-unique_clusters = enc.classes_
 sets = enc.fit_transform(clusters)
+unique_clusters = enc.classes_
 
-upset_data = {}
+# example = from_memberships(
+#      [[],
+#       ['set2'],
+#       ['set1'],
+#       ['set1', 'set2'],
+#       ['set0'],
+#       ['set0', 'set2'],
+#       ['set0', 'set1'],
+#       ['set0', 'set1', 'set2'],
+#       ],
+#       data=[56, 283, 1279, 5882, 24, 90, 429, 1957])
 
-for c in unique_clusters:
-    upset_data[c] = pd.DataFrame(columns=['Title'])
+def upset(index):
+    selection = clusters[np.where(sets[:, index] > 0 )]
+    items, counts = np.unique(selection, return_counts=True)
 
-for index, vec in enumerate(sets):
-    temp = unique_clusters[vec.astype(bool)]
-    for cluster in temp:
-        df = upset_data[cluster]
-        df = df.append({
-            "Title": meta.iloc[index]["Title"]
-            }, ignore_index=True)
-        upset_data[cluster] = df
+    subset = from_memberships(items, counts)
 
+    print("Root Class: ", unique_clusters[index])
+    plot(subset)
 
-subset = from_memberships(clusters)
-subset[0]
-plot(subset)
+upset(1)
